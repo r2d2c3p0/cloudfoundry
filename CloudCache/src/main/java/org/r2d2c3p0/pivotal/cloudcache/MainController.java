ad940fb748aa965b7d034f855b8b6b07757a2aea
R2D2-C3P0 <r2d2c3p0@users.noreply.github.com>
2018-04-29 17:42:57 -0500

    Add files via upload
    Adding get VCAP Application and VCAP Services.

--- CloudCache/src/main/java/org/r2d2c3p0/pivotal/cloudcache/MainController.java
+++ CloudCache/src/main/java/org/r2d2c3p0/pivotal/cloudcache/MainController.java
@@ -1,5 +1,6 @@
 package org.r2d2c3p0.pivotal.cloudcache;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,18 +8,47 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.*;
 
 @Controller
 public class MainController {
 
+    private final EnvironmentHelper environmentHelper;
+
+    public MainController(EnvironmentHelper environmentHelper) {
+        this.environmentHelper = environmentHelper;
+    }
+
     @RequestMapping("/")
-    public String team(Model model, HttpServletRequest request) {
+    public String team(Model model, HttpServletRequest request) throws Exception {
+
+        Map<String, Object> modelMap = new HashMap<>();
 
         String name = (String) request.getSession().getAttribute("name");
+        String instanceAddr = System.getenv("CF_INSTANCE_ADDR");
+        if (instanceAddr == null) {
+            instanceAddr = "localhost";
+        }
+
+        String containerAddr;
+        if (System.getenv("PORT") == null) {
+            containerAddr= "localhost";
+        } else {
+            containerAddr=request.getLocalAddr() + ":" + request.getLocalPort();
+        }
+
+        model.addAttribute("instanceAddr", instanceAddr);
+        model.addAttribute("containerAddr", containerAddr);
+
+        Map<String, ?> services = getVcapServicesMap();
+        services = parseServices(services);
+        modelMap.put("applicationServices", services);
+
         if (name == null) {
             return "login";
         } else {
             model.addAttribute("name", name);
+
             return "team";
         }
     }
@@ -33,9 +63,42 @@ public class MainController {
 
     }
 
+    private Map<String, Object> parseServices(Map<String, ?> services) {
+        Map<String, Object> servicesMap = Collections.synchronizedMap(new LinkedHashMap<String, Object>());
+        for (Map.Entry<String, ?> entry : services.entrySet()) {
+            List list = (List) entry.getValue();
+
+            for (Object object : list) {
+                Map map = (Map) object;
+                //weird delimiter and UUID is to deal with multiple services of the same type
+                servicesMap.put(entry.getKey() + "~~~" + UUID.randomUUID().toString(), map.get("name"));
+            }
+        }
+        return servicesMap;
+
+    }
+
     @RequestMapping("/kill")
     public void kill() {
         System.exit(1);
     }
 
+
+    private Map<String, ?> getVcapServicesMap() throws Exception {
+        return getEnvMap("VCAP_SERVICES");
+    }
+
+    private Map<String, ?> getEnvMap(String vcap) throws Exception {
+        String vcapEnv = System.getenv(vcap);
+        ObjectMapper mapper = new ObjectMapper();
+
+        if (vcapEnv != null) {
+            @SuppressWarnings("unchecked")
+            Map<String, ?> vcapMap = mapper.readValue(vcapEnv, Map.class);
+
+            return vcapMap;
+        }
+
+        return new HashMap<String, String>();
+    }
 }
--- CloudCache/src/test/java/org/r2d2c3p0/pivotal/cloudcache/CloudCacheApplicationTests.java
+++ CloudCache/src/test/java/org/r2d2c3p0/pivotal/cloudcache/CloudCacheApplicationTests.java
@@ -1,49 +1,9 @@
 package org.r2d2c3p0.pivotal.cloudcache;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
-
-import static org.hamcrest.Matchers.containsString;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-
-@RunWith(SpringRunner.class)
-@SpringBootTest
-@AutoConfigureMockMvc
 public class CloudCacheApplicationTests {
 
-    @Autowired
-    private MockMvc mockMvc;
-
-    /*@Test
-    public void homePage() throws Exception {
-        mockMvc.perform(get("/index.html"))
-                .andExpect(content().string(containsString("Home page")));
-    }
-    */
-
-    @Test
-    public void teamNoToken() throws Exception {
-        mockMvc.perform(get("/"))
-                .andExpect(content().string(containsString("Login")));
-    }
-
-    @Test
-    public void teamWithUser() throws Exception {
-        mockMvc.perform(post("/team").param("name", "Dallas Cowboys"))
-                .andExpect(content().string(containsString("Team selected: Dallas Cowboys!")));
-    }
-
-    @Test
-    public void teamPostLogin() throws Exception {
-        mockMvc.perform(get("/").sessionAttr("name", "NE Patriots"))
-                .andExpect(content().string(containsString("Team selected: NE Patriots")));
+    public static void main(String[] args) {
+        System.out.print("Hello");
     }
 
 }
